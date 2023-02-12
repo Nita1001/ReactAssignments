9eed9b4b4c3ee08ff9c7b71fb5a8fa00855ff316
Nita1001 <devinedevops@gmail.com>
2023-02-12 22:49:12 +0200

    done

---
+++ exercise4.1/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Box1 from './components/Box1.js';
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Box1/>
+      </header>
+    </div>
+  );
+
+}
+
+export default App;
