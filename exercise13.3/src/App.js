d56638066e99e5c2d9c28d1d324fe7889822ed2f
Nita1001 <devinedevops@gmail.com>
2023-02-21 02:40:00 +0200

    completed

---
+++ exercise13.3/src/App.js
@@ -0,0 +1,43 @@
+import { useState, useEffect, useRef } from 'react';
+import './App.css';
+
+function App() {
+  const [counter, setCounter] = useState(0);
+  const [color, setColor] = useState('');
+  const colors = ['color1', 'color2', 'color3', 'color4', 'color5'];
+  const colorRef = useRef(color);
+  const currentColor = counter > 5 ? 'circle' : colorRef.current;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (counter <= 5) {
+        setTimeout(() => {
+          setCounter(prevCounter => prevCounter + 1);
+          setColor(`box ${colors[counter]}`);
+          colorRef.current = `box ${colors[counter]}`;
+        }, 250);
+      } else {
+        colorRef.current = 'circle';
+      }
+    }, 500);
+    return () => clearInterval(interval);
+  }, [counter, colors, setColor]);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='container'>
+          <div className={`box ${currentColor}`}>
+
+            {counter > 0 && counter <= 5 && (
+              <div className="counter">{counter}</div>
+            )}
+
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
